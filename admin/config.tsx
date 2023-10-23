e3bd7c6a53349b1df57c1ead48d3e4b8c34244e8
rezaa27 <reza.adams@avenutech.com>
2023-10-23 18:57:06 +0200

    logo test

--- admin/config.tsx
+++ admin/config.tsx
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 function CustomLogo () {
-  let url = `${process.env.NODE_ENV==='production'? process.env.RAILWAY_VOLUME_MOUNT_PATH + '/thecptlocal.png':'../images/thecptlocal.png'}`
+  let url = `${process.env.NODE_ENV==='production'? '/thecptlocal.png':'../images/thecptlocal.png'}`
     return <img width="auto" height="100" style={{marginLeft:"-20px"}} src={url} />
 }
 
