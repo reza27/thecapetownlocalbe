61deb3862d2096da27f0bf774ba89163ef395507
rezaa27 <reza.adams@avenutech.com>
2023-10-23 17:42:03 +0200

    logo

--- admin/config.tsx
+++ admin/config.tsx
@@ -1,8 +1,8 @@
 
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 function CustomLogo () {
-  let url = `${process.env.NODE_ENV==='production'? process.env.RAILWAY_VOLUME_MOUNT_PATH + '/thecptlocal2.png':'../images/thecptlocal2.png'}`
+  let url = `${process.env.NODE_ENV==='production'? process.env.RAILWAY_VOLUME_MOUNT_PATH + '/thecptlocal.png':'../images/thecptlocal.png'}`
     return <img width="auto" height="100" style={{marginLeft:"-20px"}} src={url} />
 }
 
